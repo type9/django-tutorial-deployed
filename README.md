ea27e0dccab4bc427ce9ca82612ab046fa20770e
Dani Roxberry <dani@bitoriented.com>
2019-12-05 13:14:06 -0800

    [add] starter

---
+++ README.md
@@ -0,0 +1,2 @@
+# django2
+Tutorial Django 2.2.7
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/droxey/dev/repos/django-tutorial-deploy/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
